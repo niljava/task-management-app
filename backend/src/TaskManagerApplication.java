8d7e06ce5a66127d6d94e3369997aa227d7aa213
Nilesh <nil.y.jadhav@gmail.com>
2026-02-17 18:02:30 +0530

    initial commit

---
+++ backend/src/TaskManagerApplication.java
@@ -0,0 +1,8 @@
+
+@SpringBootApplication
+public class TaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerApplication.class, args);
+    }
+}
---
+++ backend/src/main/java/com/example/taskmanager/entity/Task.java
@@ -0,0 +1,43 @@
+package main.java.com.example.taskmanager.entity;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean isCompleted = false;
+
+    private LocalDateTime dueDate;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String assignedTo;
+
+    @PrePersist
+    void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
