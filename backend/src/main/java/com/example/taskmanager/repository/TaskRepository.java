d71bc49e0734aff4ef490d26ade35dadbb803c21
niljava <103238366+niljava@users.noreply.github.com>
2026-02-17 18:21:10 +0530

    Create TaskRepository.java

---
+++ backend/src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.example.taskmanager.repository;
+
+import com.example.taskmanager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository
+        extends JpaRepository<Task, Integer>, JpaSpecificationExecutor<Task> {
+}
